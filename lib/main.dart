fc7afa3581fe1941a76385ab211c2b41f0178e67
naseeb <naseebnaushad46@gmail.com>
2025-08-13 22:11:32 +0530

    Changed to Online Tally counter

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Online Counter',
+      title: 'Online Tally Counter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
